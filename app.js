9151c3178e45d0917cd3c03a83c23b21bc5ec41e
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-04-20 21:03:19 -0400

    Add Morgan and custom middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 
 const app = express();
@@ -8,12 +9,21 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('layout', 'layouts/main-layout.ejs');
 
+
+app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use(expressLayouts);
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  console.log('MY MIDDLEWARE 😎🤓');
+  req.pizza = 'Pizza 🍕';
+  next();
+});
+
 
 app.get('/', (req, res, next) => {
+  console.log('OMG req.pizza', req.pizza);
   res.render('home-view.ejs');
 });
 
